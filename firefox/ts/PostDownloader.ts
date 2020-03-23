273fe8ad2c847934249114d534b60082f9374731
HuiiBuh <nhaderer1@gmail.com>
2020-03-23 21:15:40 +0100

    Added the post download button

--- firefox/ts/PostDownloader.ts
+++ firefox/ts/PostDownloader.ts
@@ -5,12 +5,13 @@ class PostDownloader {
         await sleep(2000);
 
         const postList: HTMLElement[] = Array.from(document.getElementsByClassName(Variables.postWrapper)) as HTMLElement[];
+        // TODO different window sizes
+
         postList.forEach((element: HTMLElement) => {
             const bookmarkElement: HTMLElement = element.getElementsByClassName(Variables.postBookmark)[0] as HTMLElement;
 
             const downloadButton: HTMLElement = document.createElement('span');
             downloadButton.setAttribute('class', 'post-download-button');
-            downloadButton.style.background = 'red';
             bookmarkElement.appendChild(downloadButton);
         });
     }
