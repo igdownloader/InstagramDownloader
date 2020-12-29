b92d1eb938cc89e24a8b6f17199cbe0a92de9801
HuiiBuh <nhaderer1@gmail.com>
2020-12-29 10:09:45 +0100

    Improved the changelog

---
+++ src/options/changelog.ts
@@ -0,0 +1,54 @@
+/****************************************************************************************
+ * Copyright (c) 2020. HuiiBuh                                                          *
+ * This file (changelog.ts) is part of InstagramDownloader which is released under      *
+ * GNU LESSER GENERAL PUBLIC LICENSE.                                                   *
+ * You are not allowed to use this code or this file for another project without        *
+ * linking to the original source AND open sourcing your code.                          *
+ ****************************************************************************************/
+
+export interface BulletPoint {
+    text: string;
+    link: string;
+}
+
+interface Version {
+    Fixes?: (BulletPoint | string)[]
+    New?: (BulletPoint | string)[]
+}
+
+export type Changelog = Record<string, Version>
+
+export const changeLog: Changelog = {
+    '4.1.1': {
+        'Fixes': ['Changed paypal link so paypal does not keep 40%'],
+    }, '4.0.0': {
+        'Fixes': [{
+            text: 'Download all button stays on top of the page #105',
+            link: 'https://github.com/HuiiBuh/InstagramDownloader/issues/105',
+        }, {
+            text: 'Customize scroll interval #106',
+            link: 'https://github.com/HuiiBuh/InstagramDownloader/issues/106',
+        }, {
+            text: 'Dowwnload correct story image #108',
+            link: 'https://github.com/HuiiBuh/InstagramDownloader/issues/108',
+        }, {
+            text: 'Fix video download #109',
+            link: 'https://github.com/HuiiBuh/InstagramDownloader/issues/109',
+        },
+            'Removed lots of unnecessary code',
+        ],
+        'New': [
+            'See the progress of you background downloads',
+            'Download button moves down when you scroll, so you can start the download from everywhere',
+            'Customize the download speed', 'Source Code bundling with Webpack',
+        ],
+    },
+    '3.4.1': {
+        'Fixes': [
+            'Download all button not appearing',
+        ],
+        'New': [
+            'Download all button on saved page',
+        ],
+    },
+};
--- src/options/options.ts
+++ src/options/options.ts
@@ -1,3 +1,35 @@
+import { BulletPoint, changeLog } from './changelog';
 import './options.scss';
 
 (document.querySelector('#year') as HTMLElement).innerText = new Date().getFullYear().toString();
+displayChangelog();
+
+function displayChangelog(): void {
+    let versionHtml = '';
+    for (const key of Object.keys(changeLog)) {
+        const version = changeLog[key];
+        versionHtml += `<h3>Version ${key}</h3>`;
+
+        if (version.New) versionHtml += append(version.New, 'New:');
+        if (version.Fixes) versionHtml += append(version.Fixes, 'Fixes');
+    }
+
+    (document.querySelector('#changelog') as HTMLDivElement).innerHTML = versionHtml;
+}
+
+// @ts-ignore
+function append(bulletPoint: (BulletPoint | string)[], type: string): string {
+    let returnHtml = `<h4>${type}</h4>`;
+    returnHtml += '<ul>';
+    bulletPoint.forEach(b => {
+        if (typeof b === 'object') {
+            returnHtml += `<li><a href="${b.link}" target="_blank">${b.text}</a></li>`;
+        } else {
+            returnHtml += `<li>${b}</li>`;
+        }
+    });
+
+    returnHtml += '</ul>';
+
+    return returnHtml;
+}
