f03a0199dd81f3699514e0622923cee86fadab24
HuiiBuh <nhaderer1@gmail.com>
2021-01-05 20:22:33 +0100

    Use native zip to improve compatibility with the chrome store

--- src/build.js
+++ src/build.js
@@ -8,7 +8,7 @@
 
 const fs = require('fs');
 const fse = require('fs-extra');
-const JSZip = require('jszip');
+const exec = require('child_process').execSync;
 
 const browserList = ["chrome", "firefox"];
 
@@ -22,14 +22,18 @@ const browserList = ["chrome", "firefox"];
 
     // Generate the extension for every browser
     for (const browser of browserList) {
-        await collectExtension(browser);
-        const zip = await saveDirectoryToZip(`zip/${browser}`);
-        zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
-            .pipe(fs.createWriteStream(`zip/${browser}.zip`));
+        await assembleExtensionFiles(browser);
+        await execute(`cd zip/${browser} && zip ../${browser}.zip * -r && cd ../..`);
     }
+
+    await execute("git archive --format zip --output zip/InstagramDownloader.zip HEAD");
 })();
 
-async function collectExtension(browser) {
+/**
+ * Collect all files necessary to build the extension
+ * @param browser The browser
+ */
+async function assembleExtensionFiles(browser) {
     const path = `zip/${browser}`;
     if (await fs.existsSync(path)) {
         await fs.promises.rmdir(path, {recursive: true});
@@ -39,54 +43,20 @@ async function collectExtension(browser) {
 
     await fs.promises.copyFile(`src/manifest_${browser}.json`, `${path}/manifest.json`);
     await fse.copy("src/icons/", `${path}/icons`);
-
     await fse.copy("dist", path);
 }
 
-/**
- * Save a directory to a zip file
- * @param zip The zip file
- * @param directory The directory
- * @param leaveRoot Should the root folder not be added as path
- * @param folderNumberToIgnore If you set leaveRoot to true how many folder iterations should be ignored
- */
-async function saveDirectoryToZip(directory, zip = null, leaveRoot = true, folderNumberToIgnore = 2) {
-
-    if (!zip) {
-        zip = new JSZip();
-    }
-
-    const files = await fs.promises.readdir(directory);
-
-    // For every file in a folder
-    for (const file of files) {
-
-        // Go through the directory and collect every file
-        if ((await fs.promises.lstat(`${directory}/${file}`)).isDirectory()) {
-            await saveDirectoryToZip(`${directory}/${file}`, zip, leaveRoot);
-        } else {
-            if (leaveRoot) {
-                let dir = directory.split("/").splice(folderNumberToIgnore, directory.length - 2).join("/");
-                if (dir) dir += "/";
-                await saveFileToZip(zip, `${directory}/${file}`, `${dir}${file}`);
-            } else {
-                await saveFileToZip(zip, `${directory}/${file}`);
-            }
-        }
-
-    }
-
-    return zip;
-}
-
 
 /**
- * Save a file to the zip
- * @param zip The zip file
- * @param fileLocation The file location
- * @param filename The file name
+ * Execute a command in the linux command line
+ * @param command The command which should be executed
  */
-async function saveFileToZip(zip, fileLocation, filename = fileLocation) {
-    const data = (await fs.promises.readFile(fileLocation));
-    zip.file(filename, data);
+function execute(command) {
+    let response = "";
+    try {
+        response = exec(command, {encoding: "utf-8"});
+    } catch (e) {
+        console.error(e);
+    }
+    return response;
 }
