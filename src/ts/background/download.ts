98e642bdcf8005bc7767d6d883c168c73486d307
Alan O'Reilly <alanoreilly@outlook.ie>
2021-05-05 13:01:49 +0100

    Fixed again

--- src/ts/background/download.ts
+++ src/ts/background/download.ts
@@ -21,7 +21,7 @@ export async function downloadSingleImage(message: DownloadMessage): Promise<voi
     try {
         downloadURL = window.URL.createObjectURL(await (await fetch(message.imageURL[0])).blob());
     } catch {
-        if ('browser' in window) headers.push({name: 'Referer', value: 'instagram.com'}, {name: 'User-Agent', value: navigator.userAgent});
+        if ('browser' in window) headers.push({name: 'Referer', value: 'instagram.com'});
     }
 
     console.log('download');
